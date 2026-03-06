def3a05e4035f6aaf5d0b93e771aed6491ea5f8b
notraschi <nomeutentedisponigfr@gmail.com>
2026-03-06 01:53:05 +0100

    selection idea

--- src/selection.rs
+++ src/selection.rs
@@ -1,5 +1,58 @@
+use std::ops::Range;
 
-// pub struct Selection {
-//     pub content : ropey::Rope
+#[derive(Default)]
+#[allow(unused)]
+pub struct Selection {
+	active: bool,
+	// both are indexes into the rope
+	anchor: usize,
+	end: usize,
+	mode: SelectionMode
+}
 
-// }
+#[allow(unused)]
+impl Selection {
+	fn range_raw(&self) -> Range<usize> {
+		self.anchor.min(self.end)..self.anchor.max(self.end)
+	}
+
+	fn ctx<'a>(&self, ctx: &'a ropey::Rope) -> ropey::RopeSlice<'a> {
+		if self.active {
+			ctx.slice(self.range_raw())
+		} else {
+			ctx.slice(0..0)
+		}
+	}
+
+	fn clone_ctx(&self, ctx: &ropey::Rope) -> ropey::Rope {
+		self.ctx(ctx).into()
+	}
+}
+
+#[allow(unused)]
+pub enum SelectionMode {
+	Line,
+	Char
+}
+
+#[allow(unused)]
+impl Default for SelectionMode {
+	fn default() -> Self {
+		SelectionMode::Char
+	}
+}
+
+mod tests {
+	use super::*;
+
+	#[test]
+	fn ctx_test() {
+		let mut s = Selection::default();
+		let c = ropey::Rope::from("123456789");
+		s.end = 4;
+		assert_eq!(s.ctx(&c), "");
+		s.active = true;
+		assert_eq!(s.ctx(&c), "1234");
+	}
+}
+
